223bc4f97b428a0b4dda1fcb0a271adc73627c72
Shuaib Salam <shuaibsalam1234@gmail.com>
2024-01-31 16:48:32 +0530

    Complete table implementation

--- UI/src/components/Table/Table.jsx
+++ UI/src/components/Table/Table.jsx
@@ -1,9 +1,55 @@
+import { useEffect, useState } from 'react';
 import './Table.scss';
+import axiosInstance from '../../api/axios';
 
 
 const Table = () => {
-  return (
-    <div>Table</div>
+
+    const [tableData,setTableData] = useState([]);
+
+    useEffect(()=>{
+        const fetchTableData = async()=>{
+         const response  = await axiosInstance.get('/api/table');
+            setTableData(response.data)
+        }
+        fetchTableData();
+    },[]);
+
+    return (
+    <div className='tableContainer'>
+     <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((row) => (
+          <tr key={row.id}>
+            <td>{row.id}</td>
+            <td>{row.name}</td>
+            <td>{row.quantity}</td>
+            <td>{row.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
+
+    <div className="profile">
+        <img className='profileIMG' src="/img/media.png" alt="profile.png" loading='lazy'/>
+        <h6>John Doe</h6>
+        <p>CEO</p>
+        <div className="icons">
+            <img src="/img/facebook.png" alt="facebook" loading='lazy'/>
+            <img src="/img/instagram.png" alt="instagram" loading='lazy'/>
+            <img src="/img/twitter.png" alt="twitter" loading='lazy'/>
+        </div>
+    </div>
+    </div>
   )
 }
 
